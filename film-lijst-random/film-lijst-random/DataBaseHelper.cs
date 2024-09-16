d4649f563bf1bbc8471eed71d28e803b6e02d868
YourSmallFriend <SamKint10@hotmail.com>
2024-09-16 13:11:41 +0200

    initial commit

---
+++ film-lijst-random/film-lijst-random/DataBaseHelper.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class DataBaseHelper
+{
+    private string ConnectionString { get; }
+
+    public DataBaseHelper(string connectionString)
+    {
+        ConnectionString = connectionString;
+    }
+
+    public List<Tuple<string, string, string, string>> GetFilm()
+    {
+        var FilmLijst = new List<Tuple<string, string, string, string>>();
+
+        using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+        {
+            string query = "SELECT film_id, title, description, rating FROM `film`";
+            MySqlCommand command = new MySqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var id = reader["film_id"].ToString();
+                    var title = reader["title"].ToString();
+                    var description = reader["description"].ToString();
+                    var rating = reader["rating"].ToString();
+                    FilmLijst.Add(Tuple.Create(id, title, description, rating));
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        return FilmLijst;
+    }
+}
---
+++ film-lijst-random/film-lijst-random/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using film_lijst_random;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace film_lijst_random
+{
+    internal class Program
+    {
+        private const string ConnectionString = "Server=localhost;Database=sakila;user=root;";
+        static void Main(string[] args)
+        {
+            DataBaseHelper dbHelper = new DataBaseHelper(ConnectionString);
+            List<Tuple<string, string, string, string>> films = dbHelper.GetFilm();
+
+            foreach (var film in films)
+            {
+                // Print each film to the console
+                Console.WriteLine($"film_id: {film.Item1}\nTitle: {film.Item2}\nDescription: {film.Item3}\nRating: {film.Item4}\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
